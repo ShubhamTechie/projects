2adfb63d27f06b0bdc771bc12331e4b698102c98
Shubham Sahu <shubham.sahu.techie@gmail.com>
2026-05-15 13:12:03 +0530

    irctc app

---
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -0,0 +1,17 @@
+package ticket.booking.entities;
+
+public class User {
+
+
+    private String name;
+
+    private String password;
+
+    private String hashPassword;
+
+    private  List<Ticket> ticketBooked;
+
+    private  String userId;
+
+
+}
